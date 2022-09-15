50f9bc48a8851bce2b6b7ce435059b927765b36e
GangMinGangMin <chgm021122@gmail.com>
2022-09-16 00:44:02 +0900

    2-5th commit

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -123,8 +123,7 @@ public class WordCRUD implements ICRUD{
                 count++;
             }
             br.close();
-            System.out.println("==> " + count + "개 로딩 완료!!!") ;
-
+            System.out.println("==>" + count + "개 로딩 완료!!!");
         } catch (IOException e) {
             e.printStackTrace();
         }
